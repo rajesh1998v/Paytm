8167a9304ca6d2cb691db5b8fa0e3607d6478d9f
Rajesh Kumar <rajesh@example.com>
2025-11-16 18:31:29 +0530

    add code

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -8,48 +8,59 @@ function Home() {
     return (
         <div className="">
             <div className="Zsef23">
-                <div className="Sdefxs">
-                    <div className="kUhgt">Recharges & Bill Payments</div>
-                    <div className="IwXon2">
-                        <div className="a5Bdew">
-                            <img src="https://assetscdn1.paytm.com/images/catalog/view_item/2839929/17630252264344684.png" alt="rech" />
-                            <span>Mobile Recharge</span>
-                        </div>
-                        <div className="a5Bdew">
-                            <img src="https://assetscdn1.paytm.com/images/catalog/view_item/2839930/24719034555061353.png" alt="rech" />
-                            <span>DTH Recharge</span>
-                        </div>
-                        <div className="a5Bdew">
-                            <img src="https://assetscdn1.paytm.com/images/catalog/view_item/2839931/17630306417008374.png" alt="rech" />
-                            <span>FasTag Recharge</span>
-                        </div>
-                        <div className="a5Bdew">
-                            <img src="https://assetscdn1.paytm.com/images/catalog/view_item/3171057/25654768955408870.png" alt="rech" />
-                            <span>Electricity Bill</span>
-                        </div>
-                        <div className="a5Bdew">
-                            <img src="https://assetscdn1.paytm.com/images/catalog/view_item/3171058/25654802470500577.png" alt="rech" />
-                            <span>Mobile Recharge</span>
-                        </div>
-                        <div className="a5Bdew">
-                            <img src="https://assetscdn1.paytm.com/images/catalog/view_item/3171059/25654906198636032.png" alt="rech" />
-                            <span>Metro Recharge</span>
-                        </div>
-                        <div className="a5Bdew">
-                            <img src="https://assetscdn1.paytm.com/images/catalog/view_item/2839929/17630252264344684.png" alt="rech" />
-                            <span>City Bus Tikets</span>
-                        </div>
-                        <div className="a5Bdew">
-                            <img src="https://assetscdn1.paytm.com/images/catalog/view_item/2839932/17630339117612605.png" alt="rech" />
-                            <span>View All Products</span>
-                        </div>
+                <div className="Sdefx">
+                    <div className="qYn3s">
+                        <div className="kUhgt">Recharges & Bill Payments</div>
+                        <div className="IwXon2">
+                            <div className="a5Bdew">
+                                <img src="https://assetscdn1.paytm.com/images/catalog/view_item/2839929/17630252264344684.png" alt="rech" />
+                                <span>Mobile Recharge</span>
+                            </div>
+                            <div className="a5Bdew">
+                                <img src="https://assetscdn1.paytm.com/images/catalog/view_item/2839930/24719034555061353.png" alt="rech" />
+                                <span>DTH Recharge</span>
+                            </div>
+                            <div className="a5Bdew">
+                                <img src="https://assetscdn1.paytm.com/images/catalog/view_item/2839931/17630306417008374.png" alt="rech" />
+                                <span>FasTag Recharge</span>
+                            </div>
+                            <div className="a5Bdew">
+                                <img src="https://assetscdn1.paytm.com/images/catalog/view_item/3171057/25654768955408870.png" alt="rech" />
+                                <span>Electricity Bill</span>
+                            </div>
+                            <div className="a5Bdew">
+                                <img src="https://assetscdn1.paytm.com/images/catalog/view_item/3171058/25654802470500577.png" alt="rech" />
+                                <span>Mobile Recharge</span>
+                            </div>
+                            <div className="a5Bdew">
+                                <img src="https://assetscdn1.paytm.com/images/catalog/view_item/3171059/25654906198636032.png" alt="rech" />
+                                <span>Metro Recharge</span>
+                            </div>
+                            <div className="a5Bdew">
+                                <img src="https://assetscdn1.paytm.com/images/catalog/view_item/2839929/17630252264344684.png" alt="rech" />
+                                <span>City Bus Tikets</span>
+                            </div>
+                            <div className="a5Bdew">
+                                <img src="https://assetscdn1.paytm.com/images/catalog/view_item/2839932/17630339117612605.png" alt="rech" />
+                                <span>View All Products</span>
+                            </div>
 
+                        </div>
                     </div>
-                </div>
 
-                <div className="Awlk32">
-                    <img src="https://assetscdn1.paytm.com/images/catalog/view_item/3171065/27389616900368101.png" alt="img1" />
+                    <div className="ZiVtr">
+                        <div className="Awlk32">
+                            <img src="https://assetscdn1.paytm.com/images/catalog/view_item/3171065/27389616900368101.png" alt="img1" />
+                        </div>
+                    </div>
+
+
                 </div>
+            </div>
+
+
+
+            <div className="Zsef23">
 
                 <div className="z9vfs">
                     <div className="kaEw2">
@@ -65,8 +76,7 @@ function Home() {
                     </div>
                 </div>
 
-                <div className="Sdefxs">
-
+                <div className="Sdefx mt-3">
                     <div className="AjDe2">
                         <div className="kdwA">
                             <img src="https://assetscdn1.paytm.com/images/catalog/view/342367/1725447383123.png" alt="bennerd" />
@@ -77,54 +87,13 @@ function Home() {
                         <div className="x1MFf">
                             Pay anyone, everywhere. Make contactless & secure payments in-stores or online using Paytm UPI or Directly from your Bank Account. Plus, send & receive money from anyone.
                         </div>
-                    </div>
-                </div>
-
-
-
-                <hr />
-                <hr />
-                <div className="Sdefxs1">
-                    <div className="Swqes">
-                        <div className="kUhgt">Recharges & Bill Payments</div>
-                        <div className="IwXon2s">
-                            <div className="a5Bdew2">
-                                <img src="https://assetscdn1.paytm.com/images/catalog/view_item/2839929/17630252264344684.png" alt="rech" />
-                                <span>Mobile Recharge</span>
-                            </div>
-                            <div className="a5Bdew2">
-                                <img src="https://assetscdn1.paytm.com/images/catalog/view_item/2839930/24719034555061353.png" alt="rech" />
-                                <span>DTH Recharge</span>
-                            </div>
-                            <div className="a5Bdew2">
-                                <img src="https://assetscdn1.paytm.com/images/catalog/view_item/2839931/17630306417008374.png" alt="rech" />
-                                <span>FasTag Recharge</span>
-                            </div>
-                            <div className="a5Bdew2">
-                                <img src="https://assetscdn1.paytm.com/images/catalog/view_item/3171057/25654768955408870.png" alt="rech" />
-                                <span>Electricity Bill</span>
-                            </div>
-                            <div className="a5Bdew2">
-                                <img src="https://assetscdn1.paytm.com/images/catalog/view_item/3171058/25654802470500577.png" alt="rech" />
-                                <span>Mobile Recharge</span>
-                            </div>
-                            <div className="a5Bdew2">
-                                <img src="https://assetscdn1.paytm.com/images/catalog/view_item/3171059/25654906198636032.png" alt="rech" />
-                                <span>Metro Recharge</span>
-                            </div>
-                            <div className="a5Bdew2">
-                                <img src="https://assetscdn1.paytm.com/images/catalog/view_item/2839929/17630252264344684.png" alt="rech" />
-                                <span>City Bus Tikets</span>
-                            </div>
-                            <div className="a5Bdew2">
-                                <img src="https://assetscdn1.paytm.com/images/catalog/view_item/2839932/17630339117612605.png" alt="rech" />
-                                <span>View All Products</span>
-                            </div>
-
+                        <div className="mM6X9">
+                            <img src="https://assetscdn1.paytm.com/images/catalog/view_item/2834703/1725432129171.png" alt="link"/>
+                            <img src="https://assetscdn1.paytm.com/images/catalog/view_item/2834704/1725432148873.png" alt="link2"/>
                         </div>
                     </div>
-
                 </div>
+
 
 
 
