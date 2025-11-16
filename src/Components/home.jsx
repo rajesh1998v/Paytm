5126dc0a1c8b780f2f29aa2156f1b539545ebd76
Rajesh Kumar <rajesh@example.com>
2025-11-16 14:50:37 +0530

    add code

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -53,34 +53,80 @@ function Home() {
 
                 <div className="z9vfs">
                     <div className="kaEw2">
-                        <img src="https://assetscdn1.paytm.com/images/catalog/view_item/3171063/25655116567472421.png" alt="banner"/>
-                        <img src="https://assetscdn1.paytm.com/images/catalog/view_item/3171064/32743882058815649.png" alt="banner2"/>
-                        
+                        <img src="https://assetscdn1.paytm.com/images/catalog/view_item/3171063/25655116567472421.png" alt="banner" />
+                        <img src="https://assetscdn1.paytm.com/images/catalog/view_item/3171064/32743882058815649.png" alt="banner2" />
+
                     </div>
                 </div>
 
                 <div className="ZyNsa">
                     <div className="PoGrf">
-                        <img src="https://assetscdn1.paytm.com/images/catalog/view_item/2988609/20668546951073032.png" alt="offer"/>
+                        <img src="https://assetscdn1.paytm.com/images/catalog/view_item/2988609/20668546951073032.png" alt="offer" />
                     </div>
                 </div>
 
                 <div className="Sdefxs">
-                   
+
                     <div className="AjDe2">
                         <div className="kdwA">
                             <img src="https://assetscdn1.paytm.com/images/catalog/view/342367/1725447383123.png" alt="bennerd" />
                         </div>
-                        
+
                         <div className="qixG2s">Pay anyone directly from your <span>bank account</span></div>
-                        
+
                         <div className="x1MFf">
                             Pay anyone, everywhere. Make contactless & secure payments in-stores or online using Paytm UPI or Directly from your Bank Account. Plus, send & receive money from anyone.
                         </div>
                     </div>
                 </div>
 
-                
+
+
+                <hr />
+                <hr />
+                <div className="Sdefxs1">
+                    <div className="Swqes">
+                        <div className="kUhgt">Recharges & Bill Payments</div>
+                        <div className="IwXon2s">
+                            <div className="a5Bdew2">
+                                <img src="https://assetscdn1.paytm.com/images/catalog/view_item/2839929/17630252264344684.png" alt="rech" />
+                                <span>Mobile Recharge</span>
+                            </div>
+                            <div className="a5Bdew2">
+                                <img src="https://assetscdn1.paytm.com/images/catalog/view_item/2839930/24719034555061353.png" alt="rech" />
+                                <span>DTH Recharge</span>
+                            </div>
+                            <div className="a5Bdew2">
+                                <img src="https://assetscdn1.paytm.com/images/catalog/view_item/2839931/17630306417008374.png" alt="rech" />
+                                <span>FasTag Recharge</span>
+                            </div>
+                            <div className="a5Bdew2">
+                                <img src="https://assetscdn1.paytm.com/images/catalog/view_item/3171057/25654768955408870.png" alt="rech" />
+                                <span>Electricity Bill</span>
+                            </div>
+                            <div className="a5Bdew2">
+                                <img src="https://assetscdn1.paytm.com/images/catalog/view_item/3171058/25654802470500577.png" alt="rech" />
+                                <span>Mobile Recharge</span>
+                            </div>
+                            <div className="a5Bdew2">
+                                <img src="https://assetscdn1.paytm.com/images/catalog/view_item/3171059/25654906198636032.png" alt="rech" />
+                                <span>Metro Recharge</span>
+                            </div>
+                            <div className="a5Bdew2">
+                                <img src="https://assetscdn1.paytm.com/images/catalog/view_item/2839929/17630252264344684.png" alt="rech" />
+                                <span>City Bus Tikets</span>
+                            </div>
+                            <div className="a5Bdew2">
+                                <img src="https://assetscdn1.paytm.com/images/catalog/view_item/2839932/17630339117612605.png" alt="rech" />
+                                <span>View All Products</span>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+
 
             </div>sss
 
