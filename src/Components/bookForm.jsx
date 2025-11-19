aa647119efa97d5437e2f3bf4be973489f4e2faf
Rajesh Kumar <rajesh@example.com>
2025-11-20 00:09:01 +0530

    add code

--- src/Components/bookForm.jsx
+++ src/Components/bookForm.jsx
@@ -83,7 +83,7 @@ function City() {
                         <button >Buy Ticket</button>
                     </div>
                     <div className="kHgT7j">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width={18} class="mb-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width={14} class="mb-">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />
                         </svg>
                         <span className="px-1">All bus tickets will be vaild for 2 hours post booking</span>
