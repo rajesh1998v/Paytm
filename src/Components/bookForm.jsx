6585f13f3bdc5311b9aeb29710688eb2a088e295
Rajesh Kumar <rajesh@example.com>
2025-11-19 22:10:34 +0530

    add code

--- src/Components/bookForm.jsx
+++ src/Components/bookForm.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React,{useState} from "react";
 import { Link } from 'react-router-dom';
 
 function City() {
+
+    const data = ["Mumbai", "Delhi", "Pune", "Jaipur", "Kolkata", "Kolkata", "Bangalore", "Bangalore", "Bangalore",];
+
+    const [value, setValue] = useState("");
+    const [filtered, setFiltered] = useState([]);
+
+    const handleChange = (e) => {
+        const newValue = e.target.value;
+        const matches = data.filter((item) =>
+            item.toLowerCase().startsWith(newValue.toLowerCase())
+        );
+        if (matches.length === 0 && newValue == "") {
+            return;
+        }
+        setValue(newValue);
+        setFiltered(newValue ? matches : []);
+    };
+
+    const handleSelect = (item) => {
+        setValue(item);
+        setFiltered([]);
+    };
+
+
 
     return (
         <div className="">
@@ -29,9 +53,30 @@ function City() {
 
             <div className="Khcd2w">Buy New Ticket</div>
 
-            
+            <div className="px-3">
+                <div style={{position: "relative" }}>
+                    <div className="input-container">
+                        <input type="text" className="input-field" placeholder=" "value={value}onChange={handleChange}/>
+                        <label className="input-label">From</label>
+                        {filtered.length > 0 && (
+                            <ul className="suggestion-box">
+                                {filtered.map((item, i) => (
+                                    <li key={i} onClick={() => handleSelect(item)}>
+                                        {item}
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
+                    </div>
 
-           
+                </div>
+
+
+            </div>
+
+
+
+
 
 
         </div>
