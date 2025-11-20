900282f2a5ddc1cb961e5d38b608f545822fd031
Rajesh Kumar <rajesh@example.com>
2025-11-20 23:11:20 +0530

    add code

--- src/Components/bookForm.jsx
+++ src/Components/bookForm.jsx
@@ -75,6 +75,25 @@ function City() {
                     <button>Buy &#x20B9; 4</button>
                 </div>
             </div>
+            <div className="Kjf5Dx">
+                <div className="Kjuh-left">
+                    <div className="pgr4e">1 Adult Ticket</div>
+                    <div className="d-flex mt-1 ">
+                        <div className="HysD7G">Simada Nature </div>
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width={16} class="mx-2 mt-2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+                            </svg>
+
+                        </div>
+                        <div className="HysD7G">S.M.V.S. Swasnndnsk</div>
+                    </div>
+
+                </div>
+                <div className="Kjuh-right">
+                    <button>Buy &#x20B9; 4</button>
+                </div>
+            </div>
 
 
             <div>
